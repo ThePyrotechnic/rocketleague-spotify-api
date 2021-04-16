175ca853fe7e72879ca1f832964b0e2316f97e32
Pyro <thepyrotechnic@tutanota.com>
2021-04-16 14:21:29 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+git_repo = "https://github.com/thepyrotechnic/rocketleague-spotify"
+
+setuptools.setup(
+    name="rocketleague-spotify-api",
+    version="0.0.0",
+    author="Michael Manis, Alec Manabat",
+    author_email="michaelmanis@tutanota.com",
+    description="Backend API for Rocket League Spotify integration",
+    license_files="LICENSE.txt",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=git_repo,
+    project_urls={
+        "Bug Tracker": f"{git_repo}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
